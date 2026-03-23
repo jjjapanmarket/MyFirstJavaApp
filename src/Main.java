7b2e20052c1f59ee2dedbe9c9de0ab1abe999f52
“Junro <jump.jun.0404@icloud.com>
2026-03-23 22:25:41 +0900

    IOからSystem.out.printlnに変更 凶からunluckyに変更

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Random;
 
 void main() {
     // 1. おみくじのリストを作る
-    String[] results = {"超大吉","大吉", "中吉", "小吉", "吉", "凶"};
+    String[] results = {"超大吉","大吉", "中吉", "小吉", "吉", "unlucky"};
 
     // 2. ランダムな数字（0〜4）を選ぶ
     int randomIndex = new Random().nextInt(results.length);
@@ -11,11 +11,11 @@ void main() {
     String result = results[randomIndex];
 
     // 4. 画面に表示する
-    IO.println("----- おみくじを引くよ！ -----");
-    IO.println("結果は... 【 " + result + " 】 です！");
+    System.out.println("----- おみくじを引くよ！ -----");
+    System.out.println("結果は... 【 " + result + " 】 です！");
 
     // 結果に合わせて一言コメント
     if (result.equals("大吉")) {
-        IO.println("最高の一日になりますね！");
+        System.out.println("最高の一日になりますね！");
     }
 }
